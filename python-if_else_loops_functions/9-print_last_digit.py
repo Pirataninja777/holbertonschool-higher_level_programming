66583b52f5473a3c158de33436375439e1fe8dff
Pirataninja777 <9483@holbertonstudents.com>
2024-09-08 21:16:49 -0400

    add 2 9-files.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    #abs()devuelve numero absoluto convierte negativo en positivo
+    print(last_digit, end="")
+    return last_digit
