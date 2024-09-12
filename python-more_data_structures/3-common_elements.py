23b58d88dd6fdcbf87cce6e49aa7fc0164ca1c76
Pirataninja777 <9483@holbertonstudents.com>
2024-09-12 09:40:12 -0400

    files 3 and 4

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    # usa la interseccion y obtiene elementos en comun
+    return set_1 & set_2
---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    # usa operacion diferencia simetrica
+    # obtiene elementos unicos
+    return set_1 ^ set_2
