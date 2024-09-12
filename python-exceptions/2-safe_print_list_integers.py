7840b7cc3622761aab4e75bdd5f646e811a4d918
Pirataninja777 <9483@holbertonstudents.com>
2024-09-12 17:50:16 -0400

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            # verificar si indice esta dentro del rango
-            if i < len(my_list):
-            # Intentamos imprimir el valor en la posición i
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-    except (ValueError, TypeError):
-        # Capturamos cualquier excepción que ocurra
-        pass
-    print()  # Imprime una nueva línea después de todos los enteros
-    return count
+    elements = 0
+    for i in range(x):
+        try:
+            # Intenta imprimir el elemento como entero
+            print("{:d}".format(my_list[i]), end="")
+            elements += 1
+        except (ValueError, TypeError):
+            # Captura el caso de índice fuera de rango
+            # y errores de tipo
+            continue
+    print("")  # Imprime un salto de línea al final
+    return elements
