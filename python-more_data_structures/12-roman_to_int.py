08f00774bec616f0076142e30cc63a5302682be5
Pirataninja777 <9483@holbertonstudents.com>
2024-09-12 16:52:25 -0400

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
         return 0
 
     # Diccionario con los valores de los números romanos
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100,
+                  'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
     # Iterar sobre la cadena en orden inverso
@@ -12,7 +13,8 @@ def roman_to_int(roman_string):
         value = roman_dict.get(char, 0)
 
         if value < prev_value:
-            # Restar si el valor actual es menor que el anterior (casos como IV, IX)
+            # Restar si el valor actual es menor
+            # que el anterior (casos como IV, IX)
             total -= value
         else:
             # Sumar si es mayor o igual
