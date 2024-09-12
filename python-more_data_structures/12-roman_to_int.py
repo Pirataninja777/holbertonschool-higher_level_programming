e8e898e1d4859b335b4478c31543b96fcf15fb69
Pirataninja777 <9483@holbertonstudents.com>
2024-09-12 16:48:32 -0400

    files 11 and 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    # Diccionario con los valores de los números romanos
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+    # Iterar sobre la cadena en orden inverso
+    for char in reversed(roman_string):
+        value = roman_dict.get(char, 0)
+
+        if value < prev_value:
+            # Restar si el valor actual es menor que el anterior (casos como IV, IX)
+            total -= value
+        else:
+            # Sumar si es mayor o igual
+            total += value
+
+        # Actualizar el valor previo
+        prev_value = value
+
+    return total
