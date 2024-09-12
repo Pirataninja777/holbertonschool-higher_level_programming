c7e8486ef9f6f94fe157737f4b385f5c91435cb0
Pirataninja777 <9483@holbertonstudents.com>
2024-09-12 09:25:10 -0400

    pycodestyle file 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,4 +3,3 @@ def search_replace(my_list, search, replace):
     # Crear una nueva lista donde se reemplazan
     # los elementos iguales a 'search' por 'replace'
     return [replace if element == search else element for element in my_list]
-
