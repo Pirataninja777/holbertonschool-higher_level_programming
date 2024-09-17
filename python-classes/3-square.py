9442b58065c3832679561bf65373753caeb66006
Pirataninja777 <9483@holbertonstudents.com>
2024-09-16 21:45:31 -0400

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-# Define una clase Square con validación de tamaño y cálculo del área.
+# Define a square with size validation and area computation.
 
 class Square:
-    # Clase que define un cuadrado con un tamaño validado y un método para calcular el área.
+    """Representa un cuadrado.
+
+    Attributes:
+        __size (int): El tamaño del cuadrado.
+    """
 
     def __init__(self, size=0):
-        # Inicializa una nueva instancia de Square.
-        #
-        # Args:
-        #     size (int, opcional): El tamaño del cuadrado. Debe ser un entero
-        #     no negativo. El valor predeterminado es 0.
-        #
-        # Raises:
-        #     TypeError: Si `size` no es un entero.
-        #     ValueError: Si `size` es menor que 0.
+        """Inicializa una nueva instancia de Square.
 
+        Args:
+            size (int, opcional): El tamaño del cuadrado. Debe ser un entero
+            no negativo. El valor predeterminado es 0.
+
+        Raises:
+            TypeError: Si `size` no es un entero.
+            ValueError: Si `size` es menor que 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -22,10 +26,10 @@ class Square:
         self.__size = size
 
     def area(self):
-        # Calcula el área del cuadrado.
-        #
-        # Returns:
-        #     int: El área del cuadrado.
+        """Calcula el área del cuadrado.
 
+        Returns:
+            int: El área del cuadrado.
+        """
         return self.__size ** 2
 
