079bff12796956d4e1c2fa82117f0acd4431671f
Pirataninja777 <9483@holbertonstudents.com>
2024-09-16 22:05:30 -0400

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-# Define una clase Square con validación de tamaño.
+"""Define una clase Square con validación de tamaño."""
 
 class Square:
-    # Clase que define un cuadrado con un tamaño validado.
+    """Clase que define un cuadrado con un tamaño validado."""
 
     def __init__(self, size=0):
-        # Inicializa una nueva instancia de Square.
+        """Inicializa una nueva instancia de Square.
 
-      # Args:
-          # size (int, opcional): El tamaño del cuadrado. Debe ser un entero
-          # no negativo. El valor predeterminado es 0.
+        Args:
+            size (int, opcional): El tamaño del cuadrado. Debe ser un entero
+            no negativo. El valor predeterminado es 0.
 
-      # Raises:
-          # TypeError: Si `size` no es un entero.
-          # ValueError: Si `size` es menor que 0.
-
+        Raises:
+            TypeError: Si `size` no es un entero.
+            ValueError: Si `size` es menor que 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
