b60745ef80365a2a085e1d2d8ba140e4c9e989e4
Pirataninja777 <9483@holbertonstudents.com>
2024-09-16 21:42:02 -0400

    pycodestyle notes added

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a square class."""
+ # Define a square class.
 class Square:
-    """Empty class that defines a square."""
+    # Empty class that defines a square.
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Define a square class with a private size attribute."""
+ # Define a square class with a private size attribute.
 
 class Square:
-    """Class that defines a square by size."""
+    # Class that defines a square by size.
 
     def __init__(self, size):
-        """Initialize a new Square instance.
+        # Initialize a new Square instance.
 
         Args:
             size (int): The size of the square.
-        """
-        self.__size = size
+
+        # self.__size = size
