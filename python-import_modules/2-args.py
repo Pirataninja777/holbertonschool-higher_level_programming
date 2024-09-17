9b5bf9d1745ffb441bea208659f14df866b702aa
Pirataninja777 <9483@holbertonstudents.com>
2024-09-17 16:38:46 -0400

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+def main():
+    argumento = len(sys.argv) - 1
+    if argumento == 0:
+        print(f"{argumento} arguments.")
+    elif argumento == 1:
+        print(f"{argumento} argument:")
+    else:
+        print(f"{argumento} arguments:")
+
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    main()
