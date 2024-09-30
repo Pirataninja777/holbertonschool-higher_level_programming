b58b5e1afbb12b549d163bdf59e662761cd308ec
Pirataninja777 <9483@holbertonstudents.com>
2024-09-30 18:02:44 -0400

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+import os
+
+"""Nombre del archivo donde se guardará la lista"""
+filename = "add_item.json"
+
+"""Inicializa la lista"""
+items = []
+
+"""Si el archivo ya existe, carga el contenido en la lista"""
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+
+"""Añade los argumentos (excluyendo
+el nombre del script) a la lista"""
+items.extend(sys.argv[1:])
+
+"""Guarda la lista en el archivo JSON"""
+save_to_json_file(items, filename)
