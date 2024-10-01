95b9e43e7c6163698587429c93d1cd955216c84d
Pirataninja777 <9483@holbertonstudents.com>
2024-10-01 13:05:30 -0400

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""crea clase Estudiante"""
+
+
+class Student:
+    """Lo mismo edad, primero y segundo nombre"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self. __dict__
+
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: self.__dict__[attr] for attr in attrs if attr in self.__dict__}
+
+        return {}
