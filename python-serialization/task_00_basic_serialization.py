40ae5e8b1826ba24fd9d939ab17aa78883896e54
Pirataninja777 <9483@holbertonstudents.com>
2024-10-07 12:45:03 -0400

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,48 +1,32 @@
 #!/usr/bin/python3
 import json
-"""basic serialization module"""
+"""Serialize"""
 
 
 def serialize_and_save_to_file(data, filename):
     """
-    Serializa un diccionario de Python y lo guarda en un archivo JSON.
+    Serializes a Python dictionary and saves it to a JSON file.
 
-    Parámetros:
-        data (dict): Un diccionario de Python con los datos a serializar.
-        filename (str): El nombre del archivo de salida JSON. Si el
-        archivo ya existe, será reemplazado.
+    Parameters:
+        data (dict): A Python dictionary containing the data to serialize.
+        filename (str): The name of the output JSON file. If the file
+        already exists, it will be replaced.
     """
-    try:
-        with open(filename, 'w', encoding='utf-8') as archivo_json:
-            json.dump(data, archivo_json, indent=4)
-        print(f"Los datos han sido serializados y guardados en '{filename}'.")
-    except Exception as e:
-        print(f"Ocurrió un error al serializar y guardar los datos: {e}")
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4)
 
 
 def load_and_deserialize(filename):
     """
-    Carga y deserializa datos desde un archivo JSON para recrear el
-    diccionario de Python original.
+    Loads and deserializes data from a JSON file to recreate the
+    original Python dictionary.
 
-    Parámetros:
-        filename (str): El nombre del archivo JSON de entrada.
+    Parameters:
+        filename (str): The name of the input JSON file.
 
-    Retorna:
-        dict: Un diccionario de Python con los datos
-        deserializados del archivo.
+    Returns:
+        dict: A Python dictionary with the deserialized
+        JSON data from the file.
     """
-    try:
-        with open(filename, 'r', encoding='utf-8') as archivo_json:
-            datos = json.load(archivo_json)
-        print(f"Datos cargados y deserializados desde '{filename}'.")
-        return datos
-    except FileNotFoundError:
-        print(f"Error: El archivo '{filename}' no fue encontrado.")
-        return {}
-    except json.JSONDecodeError:
-        print(f"Error: El archivo '{filename}' no contiene un JSON válido.")
-        return {}
-    except Exception as e:
-        print(f"Ocurrió un error al cargar y deserializar los datos: {e}")
-        return {}
+    with open(filename, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
