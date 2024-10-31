70c14b720e60491aa1804fed70b653f8cf092596
Pirataninja777 <9483@holbertonstudents.com>
2024-10-31 09:29:22 -0400

    archivos 10-12

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Display all records 2nd table ordenada by score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- updat Bob score to 10
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
