52f08bb6bc4c2fbaf7692027e22d6c15a493b1db
Pirataninja777 <9483@holbertonstudents.com>
2024-11-01 11:48:24 -0400

    1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user user_0d_1 with a password if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes to ensure the user has the granted privileges
+FLUSH PRIVILEGES;
