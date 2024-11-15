7071765b3fdf7d1d569eb307a4a85d733dc6be3c
Pirataninja777 <9483@holbertonstudents.com>
2024-11-15 09:46:18 -0400

    12-no_genre.sql

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,15 @@
+-- lista los programas sin genero asociado
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.show_id
+WHERE
+    tv_show_genres.genre_id IS NULL
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
