a76c4305731f1d6f2458376058382d44a9bd9112
Pirataninja777 <9483@holbertonstudents.com>
2024-11-19 13:29:31 -0400

    empty files

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+List all states from database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_password,
+        db=mysql_db
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    # Cerrar el cursor y la conexión a la base de datos
+    cursor.close()
+    db.close()
